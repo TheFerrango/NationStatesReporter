9337d736e8e20c2c9f4d780d1dcd43fa45e1114a
TheFerrango <llorenzo@outlook.com>
2020-01-12 18:31:08 +0100

    now generates part of the template

---
+++ Models/ComparisonDTO.cs
@@ -0,0 +1,29 @@
+namespace nsreporter.Models
+{
+    internal class ComparisonDTO
+    {
+        public ComparisonDTO()
+        {
+        }
+
+        public int CensusID { get; set; }
+        public double NewScore { get; set; }
+        public double OldScore { get; set; }
+
+        public double PercentageChange
+        {
+            get
+            {
+                return ((NewScore - OldScore)/OldScore)*100;
+            }
+        }
+
+        public double AbsoluteChange
+        {
+            get
+            {
+                return NewScore - OldScore;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using nsreporter.ReporterDB;
 using nsreporter.ReporterDB.Models;
 using System.Linq;
 using System.Collections.Generic;
+using nsreporter.Models;
 
 namespace nsreporter
 {
@@ -12,64 +13,87 @@ namespace nsreporter
     {
         static void Main(string[] args)
         {
-        //    GetLatestData();
-        CompareOldData();
+            //    GetLatestData();
+            CompareOldData();
         }
 
         private static void CompareOldData()
         {
-            var toExclude = new List<CensusEnum>(){ CensusEnum.WorldAssemblyEndorsements, CensusEnum.Survivors, CensusEnum.Population, CensusEnum.Zombies, CensusEnum.Dead };
-            DateTime oggi = new DateTime(2020,1,11);
+            var toExclude = new List<CensusEnum>() { CensusEnum.WorldAssemblyEndorsements, CensusEnum.Survivors, CensusEnum.Population, CensusEnum.Zombies, CensusEnum.Dead };
+            DateTime oggi = new DateTime(2020, 1, 11);
             DateTime meseScorso = oggi.AddMonths(-1);
             using (ReporterContext rctx = new ReporterContext())
             {
                 var datiOdierni = rctx.DatedScales
-                                      .Where(x=> !toExclude.Contains((CensusEnum)x.CensusId))
-                                      .Where(x=> x.CensusDate == oggi)
+                                      .Where(x => !toExclude.Contains((CensusEnum)x.CensusId))
+                                      .Where(x => x.CensusDate == oggi)
                                       .ToList();
                 var datiVecchi = rctx.DatedScales
-                                      .Where(x=> !toExclude.Contains((CensusEnum)x.CensusId))
+                                      .Where(x => !toExclude.Contains((CensusEnum)x.CensusId))
                                       .ToList();
-                datiVecchi = datiVecchi.Where(x=> x.CensusDate.Date == meseScorso).ToList();
+                datiVecchi = datiVecchi.Where(x => x.CensusDate.Date == meseScorso).ToList();
                 var confronto = (from tdy in datiOdierni
-                                join old in datiVecchi on tdy.CensusId equals old.CensusId
-                                select new 
-                                {
-                                    CensusID = tdy.CensusId,
-                                    NewScore = tdy.Score,
-                                    OldScore = old.Score
-                                }).ToList();
+                                 join old in datiVecchi on tdy.CensusId equals old.CensusId                                                                  
+                                 select new ComparisonDTO()
+                                 {
+                                     CensusID = tdy.CensusId,
+                                     NewScore = tdy.Score,
+                                     OldScore = old.Score
+                                 }).ToList();
 
-                var migliorati = confronto.OrderByDescending(x=>x.NewScore - x.OldScore).Take(10).ToList();
-                var peggiorati = confronto.OrderBy(x=>x.NewScore - x.OldScore).Take(10).ToList();
-                var migliori = confronto.OrderByDescending(x=>x.NewScore).Take(10).ToList();
-                var peggiori = confronto.OrderBy(x=>x.NewScore).Take(10).ToList();
+                //removes stable to 0 data because no change happened and it's not worth mentioning
+                confronto = confronto.Except(confronto.Where(x=> x.NewScore == x.OldScore && x.NewScore == 0)).ToList();
 
-                Console.WriteLine("I 10 miglioramenti maggiori sono:");
-                string tmplChange = "{0}: old value: {1:0.00}, new value: {2:0.00} (chg: {3:0.00})";
+                var migliorati = confronto.OrderByDescending(x => x.PercentageChange).Take(10).ToList();
+                var peggiorati = confronto.OrderBy(x => x.PercentageChange).Take(10).ToList();
+                var miglioratiAss = confronto.OrderByDescending(x => x.AbsoluteChange).Take(10).ToList();
+                var peggioratiAss = confronto.OrderBy(x => x.AbsoluteChange).Take(10).ToList();
+                var migliori = confronto.OrderByDescending(x => x.NewScore).Take(10).ToList();
+                var peggiori = confronto.OrderBy(x => x.NewScore).Take(10).ToList();
+
+                Console.WriteLine("I 10 miglioramenti maggiori (%) sono:");
+                string tmplChange = "[tr][td]{0}[/td][td]{1:0.00}[/td][td]{2:0.00}[/td][td]{3:0.00}[/td][td]{4:0.00}[/td][/tr]";
+
                 foreach (var item in migliorati)
                 {
-                    var perc = Math.Round(((item.NewScore-item.OldScore) / item.OldScore)*100,2);    
-                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, perc);
+                    var perc = Math.Round(item.PercentageChange, 2);
+                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, item.AbsoluteChange, perc);
                 }
 
-                Console.WriteLine("I 10 peggioramenti maggiori sono:");
+                Console.WriteLine("I 10 peggioramenti maggiori (%) sono:");
                 foreach (var item in peggiorati)
                 {
-                    var perc = Math.Round(((item.NewScore-item.OldScore) / item.OldScore)*100,2);                    
-                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, perc);
+                    var perc = Math.Round(item.PercentageChange, 2);
+                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, item.AbsoluteChange, perc);
                 }
 
-                 Console.WriteLine("I 10 migliori sono:");
+                Console.WriteLine("I 10 miglioramenti maggiori (abs) sono:");
+                foreach (var item in miglioratiAss)
+                {
+                    var perc = Math.Round(item.PercentageChange, 2);
+                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, item.AbsoluteChange, perc);
+                }
+
+                Console.WriteLine("I 10 peggioramenti maggiori (abs) sono:");
+                foreach (var item in peggioratiAss)
+                {
+                    var perc = Math.Round(item.PercentageChange, 2);
+                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, item.AbsoluteChange, perc);
+                }
+
+                Console.WriteLine("I 10 migliori sono:");
                 foreach (var item in migliori)
                 {
-                    Console.WriteLine($"{((CensusEnum)item.CensusID)}: {item.NewScore}");
+                                        var perc = Math.Round(item.PercentageChange, 2);
+                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, item.AbsoluteChange, perc);
+
                 }
 
                 Console.WriteLine("I 10 peggiori sono:");
                 foreach (var item in peggiori)
                 {
-                    Console.WriteLine($"{((CensusEnum)item.CensusID)}: {item.NewScore}");
+                   var perc = Math.Round(item.PercentageChange, 2);
+                    Console.WriteLine(tmplChange, ((CensusEnum)item.CensusID), item.OldScore, item.NewScore, item.AbsoluteChange, perc);
                 }
             }
         }
@@ -93,7 +117,7 @@ namespace nsreporter
                         Score = cens.Score,
                         Rank = cens.Rank,
                         RRank = cens.RRank
-                    });                    
+                    });
                 }
                 rctx.SaveChanges();
             }
